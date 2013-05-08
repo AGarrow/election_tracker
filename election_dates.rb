afccaebd229ea24266fb6342396cd34a5e2b3547
iProcrastinate <alexio2@mac.com>
2013-05-08 09:57:01 -0400

    outputting to csv

--- election_dates.rb
+++ election_dates.rb
@@ -1,21 +1,20 @@
 require 'date'
 require 'json'
+require 'csv'
 require 'open-uri'
 
 require 'nokogiri'
-
-CAN_GOV_URL = "http://www.psc-cfp.gc.ca/plac-acpl/leave-conge/ann2-eng.htm"
+ 
 WIKI_BASE_URL = "http://en.wikipedia.org"
 WIKI_URL	= "/wiki/Canadian_electoral_calendar"
-MUNI_URL	= "http://www.icurr.org/research/municipal_facts/Elections/index.php"
-
+ 
 MONTHS = ["January" , "February" , "March" , "April" , "May" , "June" , "July" , "August" , "September" , "October" , "November" , "December"]
 
-@elections = []
+elections = []
 
 ##### Parse Canadian Government Site ####
 
-doc = Nokogiri::HTML(open(CAN_GOV_URL))
+doc = Nokogiri::HTML(open("http://www.psc-cfp.gc.ca/plac-acpl/leave-conge/ann2-eng.htm"))
 doc.xpath('//tr').each do |row|
 	next if row.children[1].text.include? "Location"
 	election = {
@@ -23,26 +22,25 @@ doc.xpath('//tr').each do |row|
 		type: row.children[3].text,
 		date: Date.parse(row.children[5].text),
 	}
-	@elections.push(election) 
+	elections.push(election) 
 end
 
 
 #### Parse Wikipedia Pages ####
 
-def parse_wiki( href , year)
+def parse_wiki(href , year)
 	doc = Nokogiri::HTML(open(WIKI_BASE_URL+href))
 	doc.xpath('//div[@id="mw-content-text"]/ul/li').each do |elem|
 		elem = elem.text
-		if  MONTHS.include? elem.split(" ")[0] && not elem.include? "leadership" 
+		if MONTHS.include? elem.split(" ")[0] && ( !elem.include? "leadership" )  
 			election = {
 				location: elem.scan(/[A-Z]\S*/)[1,2].join(" ").gsub("Federal","").strip,
 				type: elem.match(/(\S* |\S* \S*-)(election|elections)/).to_s,
 				date: Date.parse(elem.split("-")[0] + year)
 			}
-			@elections.push(election)
+			elections.push(election)
 		end 
 	end
-
 end
 
 doc = Nokogiri::HTML(open(WIKI_BASE_URL+WIKI_URL))
@@ -53,7 +51,7 @@ end
 
 #### Parse muniscope ####
 
-doc = Nokogiri::HTML(open(MUNI_URL))
+doc = Nokogiri::HTML(open("http://www.icurr.org/research/municipal_facts/Elections/index.php"))
 doc.xpath('//table/tbody//tr').each do |row|
 	location = row.at_xpath('.//td[@class="lcell"]').text
 	
@@ -65,16 +63,25 @@ doc.xpath('//table/tbody//tr').each do |row|
 	row_data.each_with_index do |data , i|
 		if MONTHS.include? data.strip.split(" ")[0] 
 			date = Date.parse(data)
-			i == 0 ? type = "municipal" : type = row_data[i-1] 
+			type = i == 0 ? "municipal" : row_data[i-1] 
 			election = {
 				location: location,
 				type: type.gsub("\n",""),
 				date: date
 			} 
-			@elections.push(election)
+			elections.push(election)
 		end
 	end
 end
+
+#### Write to a CSV ####
+elections = elections.to_json
+CSV.open("elections.csv","w") do |csv|
+	JSON.parse(elections).each do |hash|
+		csv << hash.values
+	end
+end
+
 
 
 
